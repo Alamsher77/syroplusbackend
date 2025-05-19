9150f8cee0ad01c5981404b0d33d68d7708d7a7f
invest7762 <investflipkart@gmail.com>
2025-05-19 16:57:52 +0530

    new flipkart add to repogetory

--- controller/flipkart/create_perchase_product_fetch.js
+++ controller/flipkart/create_perchase_product_fetch.js
@@ -2,7 +2,8 @@
 import create_perchase_product_model from '../../models/create_perchase_product_model.js'
 const create_perchase_product_fetch = async (req,res)=>{
    try {
-  const products = await create_perchase_product_model.find({})
+     
+  const products = await create_perchase_product_model.find({userId:req.userId.userId})
    
   res.json(products)
    } catch (e) {
